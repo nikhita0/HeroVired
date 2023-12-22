ea59325b5210708f81cd4478027d81ccd6020597
nikhita <uppaladinninikhita@gmail.com>
2023-12-22 16:12:34 +0530

    MergeSortedLinkedLists

---
+++ Java_assaignment/MergeSortedLinkedLists.java
@@ -0,0 +1,66 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class MergeSortedLinkedLists {
+
+    public static ListNode mergeSortedLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+
+        // If one of the lists is not exhausted, append the remaining elements
+        if (l1 != null) {
+            current.next = l1;
+        } else {
+            current.next = l2;
+        }
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(3);
+        list1.next.next = new ListNode(5);
+
+        ListNode list2 = new ListNode(2);
+        list2.next = new ListNode(4);
+        list2.next.next = new ListNode(6);
+
+        System.out.println("List 1: ");
+        printList(list1);
+
+        System.out.println("List 2: ");
+        printList(list2);
+
+        ListNode mergedList = mergeSortedLists(list1, list2);
+
+        System.out.println("Merged List: ");
+        printList(mergedList);
+    }
+}
